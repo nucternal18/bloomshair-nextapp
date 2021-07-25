d6843ac280446fbbaf1b83c4e4d2709483cb2e0a
A Oladipupo-Usoro <aolausoro@gmail.com>
2021-07-25 14:35:30 +0100

    bloomshair-nextapp: add coverage threshold

--- jest.config.js
+++ jest.config.js
@@ -1,12 +1,20 @@
 /** @type {import('@ts-jest/dist/types').InitialOptionsTsJest} */
 module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'jsdom',
+  preset: "ts-jest",
+  testEnvironment: "jsdom",
   globals: {
-    'ts-jest': {
+    "ts-jest": {
       // ts-jest configuration goes here
-      tsconfig: 'tsconfig.jest.json',
+      tsconfig: "tsconfig.jest.json",
     },
   },
-  setupFilesAfterEnv: ['./jest.setup.ts']
+  setupFilesAfterEnv: ["./jest.setup.ts"],
+  coverageThreshold: {
+    global: {
+      branches: 100,
+      functions: 100,
+      lines: 100,
+      statements: 100,
+    },
+  },
 };
