d06077d04990446cebbd34f54b7ecf0b95c4dfcc
A Oladipupo-Usoro <aolausoro@gmail.com>
2021-07-31 14:10:13 +0100

    bloomshair-nextapp: updated bloomshair.yml file and added button type to button component in index.js

--- pages/index.tsx
+++ pages/index.tsx
@@ -45,7 +45,7 @@ export default function Home(): JSX.Element {
                 <p className="mb-4 text-3xl font-semibold text-gray-300 md:text-5xl lg:text-7xl">
                   Welcome to Blooms Hair
                 </p>
-                <Button color="yellow">
+                <Button type="button" color="yellow">
                   <Link href="/book-online">
                     <a>Book Now!</a>
                   </Link>
