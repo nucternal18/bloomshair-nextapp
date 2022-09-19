5ce97fa2f4d9a5e6497a5d35dfb24876d8a0e976
A Oladipupo-Usoro <aolausoro@gmail.com>
2022-09-19 23:29:45 +0100

    bloomshair-nextapp: fixed type error in useLocalStorage Hook.

--- hooks/useLocalStorage.tsx
+++ hooks/useLocalStorage.tsx
@@ -1,8 +1,8 @@
 import React from "react";
 
-const useLocalStorage = (storageKey: string, fallbackState) => {
+const useLocalStorage = (storageKey: string, fallbackState: any) => {
   const [value, setValue] = React.useState(
-    JSON.parse(localStorage.getItem(storageKey)) || fallbackState
+    JSON.parse(localStorage.getItem(storageKey) as string) || fallbackState
   );
 
   React.useEffect(() => {
