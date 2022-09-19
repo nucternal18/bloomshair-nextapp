5957e9849e4072f4ae37cde654ac72f1be7b9c1b
A Oladipupo-Usoro <aolausoro@gmail.com>
2022-09-19 22:52:15 +0100

    bloomshair-nextapp: fixed type error in error message component.

--- components/ErrorMessage.tsx
+++ components/ErrorMessage.tsx
@@ -1,5 +1,18 @@
-const ErrorMessage = ({ children, variant }) => {
-  const color = {
+import React from "react";
+
+type Color = {
+  success: string;
+  danger: string;
+  default: string;
+};
+const ErrorMessage = ({
+  children,
+  variant,
+}: {
+  children: React.ReactNode;
+  variant: keyof Color;
+}) => {
+  const color: Color = {
     success: "text-green-500 text-sm",
     danger: "text-red-500 text-sm",
     default: "text-yellow-500 text-sm",
